7805d306e61db5d69781fe598eb6e732f5db1eb9
Matias Fanger <maattz@outlook.com>
2023-06-29 16:54:57 -0300

    add: detailed auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,14 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const token = useCookie('token').value
 
+    // if the user is logged in, redirect to home page if they try to access login page
     if (to.path === '/login') {
         if (token) {
             return navigateTo('/')
         }
     }
 
+    // if the user is not logged in, redirect to login page if they try to access movie page
     if (to.path.includes('/movie')) {
         if (!token) {
             return navigateTo('/login')
